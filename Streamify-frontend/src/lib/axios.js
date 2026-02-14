660fcb00a07c67a5206fce18991ff597c3d45627
dineshkumar-mb <dineshkumarmannibrundha@gmail.com>
2026-02-14 12:09:44 +0530

    updated

--- Streamify-frontend/src/lib/axios.js
+++ Streamify-frontend/src/lib/axios.js
@@ -6,3 +6,11 @@ export const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true, // send cookies with the request
 });
+
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
