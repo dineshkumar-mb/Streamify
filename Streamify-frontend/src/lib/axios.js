b646625b29451e3cb3755289ac091930ee697933
dineshkumar-mb <dineshkumarmannibrundha@gmail.com>
2026-02-18 21:21:52 +0530

    updated axios

--- Streamify-frontend/src/lib/axios.js
+++ Streamify-frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+export const BASE_URL = import.meta.env.MODE === "development"
+  ? "http://localhost:5001/api"
+  : "https://streamify-ncde.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
